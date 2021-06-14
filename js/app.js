7b34b37f4f6c434e17b6b838a7d1640460fdf97a
MelissaLaas <melissa.laas16@gmail.com>
2021-06-14 23:41:04 +0400

    loop function added

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
-baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');
+window.addEventListener('load', function() {
+    baguetteBox.run('.gallery');
+});
+
+let input = document.querySelector('input');
+let photos = document.getElementsByTagName('a');
+
+const filter = () => {
+    let x = input.value.toLowerCase();
+
+    for (let i = 0; i < photos.length; i++){
+      let title = photos[i].getAttribute('data-caption').toLowerCase();
+
+      if (title.includes(x)) {
+        photos[i].style.display = 'block';
+      } else {
+        photos[i].style.display = 'none';
+      }
+    }
+  };
+
+ input.addEventListener('keyup', filter);  
